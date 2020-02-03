398e2b44eb8466ac94786c3fdb58043cb93ab365
Nolan <noltron000@gmail.com>
2020-02-03 14:27:27 -0800

    [add] example

--- problem01.py
+++ problem01.py
@@ -1,3 +1,9 @@
 # == TASK ==
 # Given an array of n numbers and a target value, t,
 # find two numbers from the array whose sum is t.
+
+# == EXAMPLE ==
+# array = [5, 3, 6, 8, 2, 4, 7]
+# (thus n = 7)
+# t = 10
+# results = [3, 7] or [6, 4] or [8, 2]
