97c135d216b030af7507c68d2eea49dec3833b85
Nolan <noltron000@gmail.com>
2020-02-03 16:30:12 -0800

    [add] pseudocode to problem 3

--- problem03.py
+++ problem03.py
@@ -9,5 +9,45 @@
 # results = [13, 6] or [4, 17] or [5, 14]
 
 # == PSEUDOCODE ==
+# create a new function called get_closest_pair
+# that takes three inputs, including two arrays of numbers,
+# (called a and b), and a target number t.
+
+# 	create a copy of array a and sort it
+
+# 	create a copy of array b and sort it
+
+# 	create a pointer to the smallest item in a,
+# 	based on the index of a or keeping track of it
+
+# 	create a pointer to the largest item in b,
+# 	based on the index of b or keeping track of it
+
+# 	loop while pointer a can get larger,
+# 	or pointer b can get smaller,
+
+# 		get the sum of pointers a and b.
+
+# 		if the sum is larger than t,
+
+# 			check if pointer a can't get larger...
+
+# 				if it can't, break the while loop.
+
+# 				otherwise, increase pointer a
+# 				to the next larger item
+
+# 		else, if the sum is smaller than t,
+
+# 			check if pointer b can't get smaller...
+
+# 				if it can't, break the while loop.
+
+# 				otherwise, decrease pointer b
+# 				to the next smaller item.
+
+# 		otherwise, if they are equal
+
+# 			that's great! we can flat out return the pair right away.
 
 # == TEST EXAMPLE ==
