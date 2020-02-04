854a58f32be3c06eacadbd00e3a66ce7debc786b
Nolan <noltron000@gmail.com>
2020-02-03 17:03:06 -0800

    [add] coded and tested problem 3

--- problem03.py
+++ problem03.py
@@ -9,59 +9,48 @@
 # results = [13, 6] or [4, 17] or [5, 14]
 
 # == PSEUDOCODE ==
-# create a new function called get_closest_pair
-# that takes three inputs, including two arrays of numbers,
-# (called a and b), and a target number t.
+def get_closest_pair(a, b, t):
+	a = a[:]
+	b = b[:]
+	a.sort()
+	b.sort()
 
-# 	create a copy of array a and sort it
+	index_a = 0
+	index_b = len(b) - 1
 
-# 	create a copy of array b and sort it
+	best_pair = [a[index_a], b[index_b]]
 
-# 	create a pointer to the smallest item in a,
-# 	based on the index of a or keeping track of it
+	while index_a <= len(a) - 1 and index_b >= 0:
+		# get current pair
+		current_pair = [a[index_a], b[index_b]]
+		# current pair is better than best pair
+		if abs(sum(best_pair) - t) > abs(sum(current_pair) - t):
+			best_pair = current_pair
 
-# 	create a pointer to the largest item in b,
-# 	based on the index of b or keeping track of it
+		# increment pointer a
+		if sum(current_pair) < t:
+			if index_a == len(a) - 1:
+				break
+			index_a += 1
 
-# 	create an array of size two containing
-# 	the current value of those two pointers.
-# 	call the array best_pair;
-# 	this array will be updated with better pairs
-# 	as this runs, based on their sum's difference to t.
+		# increment pointer b
+		elif sum(current_pair) > t:
+			if index_b == 0:
+				break
+			index_b -= 1
 
-# 	loop while pointer a can get larger,
-# 	or pointer b can get smaller,
+		# found an exact match;
+		# sum(current_pair) == t
+		else:
+			break
 
-# 		get the sum of pointers a and b.
-
-# 		check if the current pointers' sum are better than
-# 		best_pair's sum.
-
-# 			if it is, replace best_pair with the new best pair.
-
-# 		if the sum is larger than t,
-
-# 			check if pointer a can't get larger...
-
-# 				if it can't, break the while loop.
-
-# 				otherwise, increase pointer a
-# 				to the next larger item
-
-# 		else, if the sum is smaller than t,
-
-# 			check if pointer b can't get smaller...
-
-# 				if it can't, break the while loop.
-
-# 				otherwise, decrease pointer b
-# 				to the next smaller item.
-
-# 		otherwise, if they are equal
-
-# 			that's great! we can flat out return the pair right away.
-
-# 	the loop broke if it ever got here.
-# 	there is no exact match so just return best_pair.
+	# best pair is found when loop is done
+	return best_pair
 
 # == TEST EXAMPLE ==
+a = [9, 13, 1, 8, 12, 4, 0, 5]
+b = [3, 17, 4, 14, 6]
+t = 20
+# results = [13, 6] or [4, 17] or [5, 14]
+
+print(get_closest_pair(a, b, t))
