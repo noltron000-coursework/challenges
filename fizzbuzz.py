de6c73250cb18dc646a2109fdb229097c83fcf69
Nolan Kovacik <noltron000@gmail.com>
2020-02-19 18:20:10 -0800

    [add] fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,35 @@
+# FizzBuzz
+# -> takes integer: start
+# -> takes integer: end
+
+# returns:
+# -> fizzbuzzified list of ranges (start...finish)
+
+def fizz_buzz(start, end):
+	# create an array:
+	# a range of integers from start to finish.
+	
+	# loop through each index in array:
+
+		# run foobar each value associated with index,
+		# and replace value with the foobar return.
+
+	# return the final array
+
+
+def foobar(number):
+	# if number divisible by 15:
+
+		# return "FizzBuzz"
+
+	# else if number divisible by 5:
+	
+		# return "Buzz"
+
+	# else if number divisible by 3:
+
+		# return "Fizz"
+
+	# else:
+	
+		# return number
