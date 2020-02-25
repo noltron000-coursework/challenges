a7315310733568a76018bd58bbd1947de6ff6f0f
Nolan Kovacik <noltron000@gmail.com>
2020-02-25 13:21:02 -0800

    [add] recursive textpad functionality

--- textpad.py
+++ textpad.py
@@ -15,56 +15,78 @@ Example execution 2:
 - output:	["gmmd", ..., "gone", ..., "good", ..., "home", ..., "hood", ..., "ioof"]
 """
 
-t9_num_to_letter = {
-	0: [
+t9_num_to_letters = {
+	'0': [
 		' '  # space-character
-	]
-	1: [
+	],
+	'1': [
 		',',
 		'.',
 		'?',
 		'!',
-	]
-	2: [
+	],
+	'2': [
 		'a',
 		'b',
 		'c',
 	],
-	3: [
+	'3': [
 		'd',
 		'e',
 		'f',
 	],
-	4: [
+	'4': [
 		'g',
 		'h',
 		'i',
 	],
-	5: [
+	'5': [
 		'j',
 		'k',
 		'l',
 	],
-	6: [
+	'6': [
 		'm',
 		'n',
 		'o',
 	],
-	7: [
+	'7': [
 		'p',
 		'q',
 		'r',
 		's',
 	],
-	8: [
+	'8': [
 		't',
 		'u',
 		'v',
 	],
-	9: [
+	'9': [
 		'w',
 		'x',
 		'y',
 		'z',
 	],
 }
+
+
+def t9_letters(rmn_string, prv_combos=None):
+	if prv_combos is None:
+		prv_combos = ['']
+	if rmn_string == '':
+		return prv_combos
+
+	num = rmn_string[0]
+	rmn_string = rmn_string[1:]
+
+	possible_letters = t9_num_to_letters[num]
+	new_combos = []
+
+	for combo in prv_combos:
+		for letter in possible_letters:
+			new_combos.append(combo + letter)
+
+	return t9_letters(rmn_string, new_combos)
+
+print(t9_letters('23'))
+print(t9_letters('4663'))
