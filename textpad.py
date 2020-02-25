a5d9b881c526c49e767b6ea15784c6009f238e46
Nolan Kovacik <noltron000@gmail.com>
2020-02-25 13:01:46 -0800

    [add] t9 textpad prompt

---
+++ textpad.py
@@ -0,0 +1,16 @@
+"""
+PROMPT
+======
+
+Given a string of digits 2-9 a user has pressed on a T9
+"old school" telephone keypad, return a list of all letter
+combinations they could have been trying to type.
+
+Example execution 1:
+- input:	t9_letters("23")
+- output:	["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
+Example execution 2:
+- input:	t9_letters("4663")
+- output:	["gmmd", ..., "gone", ..., "good", ..., "home", ..., "hood", ..., "ioof"]
+"""
