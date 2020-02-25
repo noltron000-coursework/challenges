1b75e5157f0d3c4eedd48c5b68cf7bd6d13308a2
Nolan Kovacik <noltron000@gmail.com>
2020-02-25 13:33:03 -0800

    [add] pseudocode for find_indexes

--- names.py
+++ names.py
@@ -10,3 +10,21 @@ Example inputs:
 Example execution:
 - find_indexes(instructors, target) => (7, 10)
 '''
+
+make_instructors = [
+	'Adriana', 'Adriana',
+	'Alan', 'Alan', 'Alan', 'Alan', 'Alan',
+	'Braus', 'Braus', 'Braus', 'Braus',
+	'Dan', 'Dan', 'Dan', 'Dan', 'Dan',
+	'Dani', 'Dani',
+	'Jess',
+	'Meredith',
+	'Milad', 'Milad',
+	'Mitchell', 'Mitchell', 'Mitchell', 'Mitchell',
+]
+
+def find_indexes(instructors, target):
+	# find location of one target occurance
+	# find location of first target occurance
+	# find location of last target occurance
+	# return locations of first and last target
