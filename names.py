1567a9ac64834eec3931a4f9260f7ccf452afa20
Nolan Kovacik <noltron000@gmail.com>
2020-02-25 17:41:35 -0800

    [add] names implemented

--- names.py
+++ names.py
@@ -11,6 +11,7 @@ Example execution:
 - find_indexes(instructors, target) => (7, 10)
 '''
 
+
 make_instructors = [
 	'Adriana', 'Adriana',
 	'Alan', 'Alan', 'Alan', 'Alan', 'Alan',
@@ -23,8 +24,54 @@ make_instructors = [
 	'Mitchell', 'Mitchell', 'Mitchell', 'Mitchell',
 ]
 
+
 def find_indexes(instructors, target):
 	# find location of one target occurance
+	search_index = binary_search(instructors, target)[0]
+	# just in case there is no value
+	if search_index == None:
+		return None, None
+
 	# find location of first target occurance
-	# find location of last target occurance
+	start_index = search_index
+	while instructors[start_index - 1] == target:
+		start_index -= 1
+	
+	# find location of final target occurance
+	end_index = search_index
+	while instructors[end_index + 1] == target:
+		end_index += 1
+		
 	# return locations of first and last target
+	return start_index, end_index
+
+
+def binary_search(instructors, target, min_index=None, max_index=None):
+	# initialize for recursive check
+	if min_index is None:
+		min_index = 0
+	if max_index is None:
+		max_index = len(instructors)
+
+	# base case: no results found
+	if min_index == max_index:
+		return None, None
+
+	med_index = (min_index + max_index) // 2
+	# get the middle of the instructors array
+	med_value = instructors[med_index]
+
+	# recursively search if needed,
+	# else return the current median
+	if target == med_value:
+		return med_index, med_value
+	elif target < med_value:
+		return binary_search(instructors, target, min_index, med_index)
+	elif target > med_value:
+		return binary_search(instructors, target, med_index+1, max_index)
+	else:
+		raise ValueError('weird!')
+
+print(find_indexes(
+	make_instructors, 'Braus'
+))
